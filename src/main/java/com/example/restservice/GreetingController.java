f6e3aeb5dc5e694c2a3078a8624a869a5075041f
Seb4sti4n27 <27sebastian4@gmail.com>
2023-07-06 22:17:01 -0500

    wsa

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hola, %s!";
+	private static final String template = "Sebastian  %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/saludo")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/codigo")
+	public Greeting greeting(@RequestParam(value = "codigo", defaultValue = "") long id) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, id));
 	}
 }
